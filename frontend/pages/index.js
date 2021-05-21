a8929e7b326248f45a488cefe569c18d40d82508
Henrik Hey <henrikheywork@gmail.com>
2021-05-20 23:50:54 -0400

    initial commit

---
+++ frontend/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Henrik Hey</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      Hello world
+    </div>
+  )
+}
